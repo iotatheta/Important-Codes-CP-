4d74897774c6cb8b2115d8ad72fe8a73cb0f394f
iotatheta <princebiswas2006@gmail.com>
2026-09-01 17:46:12 +0530

    .

---
+++ Imp codes(CP)/Binary Search/Upper_bound.cpp
@@ -0,0 +1,71 @@
+// Upper bound = The smallest number that is > x
+// Applicable when the search space is sorted
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define ll long long
+#define endl '\n'
+
+ll upper_bound_index(const vector<ll> &arr, const ll &x)
+{
+    ll low = 0, high = (ll) arr.size() - 1, mid, ans = -1;
+    
+    while(low <= high)
+    {
+        mid = low + ((high - low) >> 1ULL);
+        
+        if(arr[mid] > x)
+        {
+            ans = mid;
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    
+    return ans;
+}
+// TC: O(log n)
+// SC: O(1)
+
+void solve()
+{
+    ll n, k, x;
+    cin >> n >> k;
+    
+    vector<ll> arr(n);
+    
+    for(ll i=0;i<n;i++)
+    {
+        cin >> arr[i];
+    }
+    
+    ll ind;
+    
+    while(k-- > 0)
+    {
+        cin >> x;
+        
+        ind = upper_bound_index(arr, x);
+        
+        if(ind == -1)
+            cout << "No upper bound" << endl;
+        else
+            cout << arr[ind] << endl;
+    }
+}
+// TC: O(n + k log n)
+// SC: O(n)
+
+int main()
+{
+    fastio();
+    solve();
+}
+// TC: O(n + k log n)
+// SC: O(n)
